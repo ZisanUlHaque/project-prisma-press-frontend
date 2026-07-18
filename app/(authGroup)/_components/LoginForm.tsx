583c2f28969584ac1f6c9aa252e962e298f307b2
Zisan <mdzisanulhaque989@gmail.com>
2026-07-18 21:32:59 +0600

    Handling Form Pending State With useActionState of ReactJS

--- app/(authGroup)/_components/LoginForm.tsx
+++ app/(authGroup)/_components/LoginForm.tsx
@@ -4,19 +4,40 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { loginAction } from "../_actions/authActions";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
 
 const LoginForm = () => {
+  const [state, action, pending] = useActionState(loginAction, false);
+  useEffect(() => {
+    if (!state) return;
+
+    if (state.success) {
+      toast.success(state.message || "Login Successfull");
+    }
+    if (!state.success) {
+      toast.error(state.message || "Login Failed");
+    }
+  }, [state]);
   return (
-    <form action={loginAction} className="space-y-4">
-        <Card className="p-5 space-y-4">
-            <Input name="email" type="email" placeholder="Enter your Email" required></Input>
-            <Input name="password" type="password" placeholder="Enter your Password" required></Input>
-            <Button type="submit">
-                Login
-            </Button>
-        </Card>
+    <form action={action} className="space-y-4">
+      <Card className="p-5 space-y-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Enter your Email"
+          required
+        ></Input>
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter your Password"
+          required
+        ></Input>
+        <Button type="submit">{pending ? "Submitting..." : "Login"}</Button>
+      </Card>
     </form>
-  )
+  );
 };
 
 export default LoginForm;
