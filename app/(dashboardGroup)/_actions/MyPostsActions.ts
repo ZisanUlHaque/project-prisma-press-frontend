0995e47ea95bcbc7dbfa35db4905eb100a7574ab
Zisan <mdzisanulhaque989@gmail.com>
2026-07-26 00:22:16 +0600

    Implementing Functionalities Of Edit News

--- app/(dashboardGroup)/_actions/MyPostsActions.ts
+++ app/(dashboardGroup)/_actions/MyPostsActions.ts
@@ -1,6 +1,114 @@
 "use server";
 
+import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
+
+type PostState = {
+    success: true,
+    statusCode: number,
+    message: string,
+    data: Record<string, unknown>
+}
+
+export const createPost = async (prevState: PostState, formData: FormData) => {
+
+  const payload = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+    thumbnail: formData.get("thumbnail"),
+    tags: (formData.get("tags") as string).split(", "),
+    isPremium: formData.get("isPremium") === "on",
+  };
+
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+
+  if (!accessToken) {
+    return {
+      success: false,
+      message: "User not logged in!!",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts`, {
+    method: "POST",
+    headers: {
+      Cookie: `accessToken=${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body : JSON.stringify(payload)
+  });
+
+  const result =await res.json();
+
+  if (result.success) {
+    revalidateTag("my-posts", {
+        expire : 0
+    })
+  }
+
+  if (result.success && result.data.isPremium) {
+    revalidateTag("premium-posts", {
+        expire : 0
+    })
+  }else{
+    revalidateTag("public-posts", {
+        expire : 0
+    })
+  }
+  return result;
+};
+
+export const updatePost = async (postId:string, prevState: PostState, formData: FormData) => {
+
+  const payload = {
+    title: formData.get("title")?? "",
+    content: formData.get("content")?? "",
+    thumbnail: formData.get("thumbnail")?? "",
+    tags: (formData.get("tags") as string).split(", ")?? "",
+    isPremium: formData.get("isPremium") === "on",
+  };
+
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+
+  if (!accessToken) {
+    return {
+      success: false,
+      message: "User not logged in!!",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts/${postId}`, {
+    method: "PATCH",
+    headers: {
+      Cookie: `accessToken=${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body : JSON.stringify(payload)
+  });
+
+  const result =await res.json();
+
+  if (result.success) {
+    revalidateTag("my-posts", {
+        expire : 0
+    })
+  }
+
+  if (result.success && result.data.isPremium) {
+    revalidateTag("premium-posts", {
+        expire : 0
+    })
+  }else{
+    revalidateTag("public-posts", {
+        expire : 0
+    })
+  }
+  return result;
+};
 
 export const getMyPosts = async () => {
   const cookieStore = await cookies();
@@ -28,4 +136,3 @@ export const getMyPosts = async () => {
   const result = res.json();
   return result;
 };
-
